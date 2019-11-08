f9dcd951b9e5383b27a78cb3c7d27a7d5f9f23b3
mdsauce <max.dobeck@saucelabs.com>
2019-11-07 17:43:59 -0800

    Added basic test and env_credential fetching function.

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+use std::env;
+
+pub fn env_credentials() -> HashMap<String, String> {
+    const USERNAME: &str = "SAUCE_USERNAME";
+    const ACCESS_KEY: &str = "SAUCE_ACCESS_KEY";
+    let sauce_username = match env::var(USERNAME) {
+        Ok(name) => name,
+        Err(e) => panic!("couldn't find {}: {}", USERNAME, e),
+    };
+    let sauce_access_key = match env::var(ACCESS_KEY) {
+        Ok(name) => name,
+        Err(e) => panic!("couldn't find {}: {}", ACCESS_KEY, e),
+    };
+    let mut creds = HashMap::new();
+    creds.insert(USERNAME.to_string(), sauce_username);
+    creds.insert(ACCESS_KEY.to_string(), sauce_access_key);
+    return creds;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn find_env_variables() {
+        use std::env;
+        env::set_var("SAUCE_USERNAME", "my.name");
+        env::set_var("SAUCE_ACCESS_KEY", "my.access.key");
+
+        assert_eq!(crate::env_credentials().len(), 2);
+        assert!(crate::env_credentials().contains_key("SAUCE_USERNAME"));
+        assert_eq!(crate::env_credentials()["SAUCE_USERNAME"], "my.name");
+    }
+}
