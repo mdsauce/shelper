fb273e260a672c6c104fe7b85ff58398d18fbba4
mdsauce <max.dobeck@saucelabs.com>
2019-11-08 17:35:02 -0800

    masked_key is not printing yet.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,29 @@
-use std::collections::HashMap;
 use std::env;
 extern crate reqwest;
 extern crate serde_json;
 
+pub struct Credentials {
+    username: String,
+    access_key: String,
+}
 
-fn env_credentials() -> HashMap<String, String> {
+fn set_credentials(user: Option<String>, key: Option<String>) -> Credentials {
+    let mut creds: Credentials = Credentials {
+        username: "default".to_string(),
+        access_key: "default".to_string(),
+    };
+    creds.username = match user {
+        Some(user) => user,
+        None => return env_credentials(),
+    };
+    creds.access_key = match key {
+        Some(key) => key,
+        None => return env_credentials(),
+    };
+    return creds;
+}
+
+fn env_credentials() -> Credentials {
     const USERNAME: &str = "SAUCE_USERNAME";
     const ACCESS_KEY: &str = "SAUCE_ACCESS_KEY";
     let sauce_username = match env::var(USERNAME) {
@@ -15,25 +34,49 @@ fn env_credentials() -> HashMap<String, String> {
         Ok(name) => name,
         Err(e) => panic!("Problem getting environment variable {}: {}", ACCESS_KEY, e),
     };
-    let mut creds = HashMap::new();
-    creds.insert(USERNAME.to_string(), sauce_username);
-    creds.insert(ACCESS_KEY.to_string(), sauce_access_key);
+    let creds: Credentials = Credentials {
+        username: sauce_username,
+        access_key: sauce_access_key,
+    };
     return creds;
 }
 
-fn all_jobs(build: &str) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
-    let creds = env_credentials();
+pub fn all_jobs(
+    build: &str,
+    user: String,
+    key: String,
+) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+    let creds = set_credentials(Some(user), Some(key));
     let build_api = format!("https://app.saucelabs.com/rest/v1/builds/{}/jobs", build);
-    let resp: serde_json::Value  = reqwest::Client::new()
-    .get(&build_api)
-    .basic_auth(&creds["SAUCE_USERNAME"], Some(&creds["SAUCE_ACCESS_KEY"]))
-    .send()?
-    .json()?;
+    let resp: serde_json::Value = reqwest::Client::new()
+        .get(&build_api)
+        .basic_auth(&creds.username, Some(&creds.access_key))
+        .send()?
+        .json()?;
+    if resp["jobs"].is_array() {
+        return Ok(resp);
+    } else {
+        println!("WE ARE HERE");
+        let mut masked_key = String::new();
+        let mut i = 0;
+        for c in creds.access_key.chars().rev() {
+            if i == 5 {
+                break;
+            }
+            &masked_key.push(c);
+            i += 1;
+            println!("{}", c);
+            println!(">>>>>>>>>>>>>>> {}", masked_key);
+        }
 
-    println!("This is the all_jobs(buildId) response headers: {:#?}", resp);
-    Ok(resp)
+        panic!(
+            "Something went wrong with the request using user {}:******{} {}.  Response: {}",
+            creds.username, masked_key, build_api, resp
+        )
+    };
 }
 
+#[cfg(test)]
 fn setup(user: &str, key: &str) {
     env::set_var("SAUCE_USERNAME", user);
     env::set_var("SAUCE_ACCESS_KEY", key);
@@ -44,14 +87,16 @@ mod tests {
     #[test]
     fn find_env_variables() {
         crate::setup("my.name", "random123125152");
-        assert_eq!(crate::env_credentials().len(), 2);
-        assert!(crate::env_credentials().contains_key("SAUCE_USERNAME"));
-        assert_eq!(crate::env_credentials()["SAUCE_USERNAME"], "my.name");
+        assert_eq!(crate::env_credentials().username, "my.name");
     }
 
     #[test]
     fn all_jobs_present() {
-        match crate::all_jobs("91ee45d589ce4177981bf22f911f22c5") {
+        match crate::all_jobs(
+            "91ee45d589ce4177981bf22f911f22c5",
+            "wrong-user1234ab12vasf".to_string(),
+            "".to_string(),
+        ) {
             Ok(resp) => assert_eq!(resp["jobs"].as_array().unwrap().len(), 32),
             Err(e) => assert_eq!(e.to_string(), ""),
         }
