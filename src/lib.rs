877b7d7b5566eb05d3baab4f900e8119664b9e2c
mdsauce <max.dobeck@saucelabs.com>
2020-02-04 14:35:21 -0800

    rest didn't make a ton of sense for a lib name.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 /// All REST API activites for saucelabs.com.  Limited to saucelabs.com APIs at the time of this writing.
 /// Not all APIs are guaranteed to be publicly listed or continue functioning.
 /// Emulator, Simulator, and Desktop test data should be accessible.
-pub mod rest;
+pub mod sauce;
